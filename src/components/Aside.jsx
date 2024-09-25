ece30fe1afd3a728681d4691d25d483a6f22e231
unknown <fahder7@gmail.com>
2024-09-26 00:22:24 +0700

    adding banner home production

---
+++ src/components/Aside.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faClipboard,
+  faPodcast,
+  faNoteSticky,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Aside = () => {
+  return (
+    <aside className="fixed bottom-8 right-8">
+      <div className="flex h-full">
+        <ul className="flex flex-col m-auto p-2 bg-slate-600 items-center justify-center gap-4 rounded-lg">
+          <li className="list-none">
+            <a href="#" className="text-3xl text-white no-underline">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </a>
+          </li>
+          <li className="list-none">
+            <a href="#" className="text-3xl text-white no-underline">
+              <FontAwesomeIcon icon={faPodcast} />
+            </a>
+          </li>
+          <li className="list-none">
+            <a href="#" className="text-3xl text-white no-underline">
+              <FontAwesomeIcon icon={faNoteSticky} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export default Aside;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Home from "../components/Home";
+import Aside from "../components/Aside";
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Aside />
+    </>
+  );
+};
+
+export default HomePage;
