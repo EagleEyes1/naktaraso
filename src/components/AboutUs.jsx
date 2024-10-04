a9a6042658726567298b2ed526f9d8d0694ed860
unknown <fahder7@gmail.com>
2024-10-04 12:59:53 +0700

    structuring aboutus

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -4,7 +4,7 @@ const AboutUs = () => {
   return (
     <>
       <div className="mx-auto px-56 z-10 bg-[#fff8ee] pt-20">
-        <div className="pt-6 grid grid-cols-2">
+        <div className="pt-20 pb-48 grid grid-cols-2">
           <div className="self-center">
             <h1 className="text-3xl font-bold pb-8">Tentang Kami</h1>
             <p className="text-lg leading-8">
@@ -21,12 +21,6 @@ const AboutUs = () => {
               kami beradaptasi dengan channel penjualan melalui e-commerce dan
               ditahun yang sama kami mampu mengirim produk ke pelanggan hampir
               di seluruh Indonesia.
-              <br /> Dengan bangga kami sampaikan bahwa produk kami telah
-              mendapatkan sertifikat halal dari LPPOM MUI. Produksi telah
-              distandardisasi sesuai Good Manufacturing Practices (GMP) dan
-              Hazar Analysis and Critical Control Point (HACCP). Setiap hari
-              kami menerapkan standardisasi tersebut untuk mencapai produk
-              berkualitas tinggi.
             </p>
           </div>
           <div className="pl-28">
@@ -39,6 +33,12 @@ const AboutUs = () => {
         <div className="pt-5 grid grid-cols-2 pb-20">
           <div>
             <p className="text-lg leading-8">
+              Dengan bangga kami sampaikan bahwa produk kami telah mendapatkan
+              sertifikat halal dari LPPOM MUI. Produksi telah distandardisasi
+              sesuai Good Manufacturing Practices (GMP) dan Hazar Analysis and
+              Critical Control Point (HACCP). Setiap hari kami menerapkan
+              standardisasi tersebut untuk mencapai produk berkualitas tinggi.
+              <br />
               <b>Sejarah Perusahaan</b> <br /> 2018 : Memulai Usaha <br />
               2019 : Mendirikan Perusahaan Perorangan <br />
               2020 : Mendirikan perusahaan berbadan hukum <br /> <br />
@@ -52,9 +52,9 @@ const AboutUs = () => {
           </div>
         </div>
       </div>
-      <div className="text-lg leading-8 bg-[#ffeedc] px-56 py-12">
+      <div className="text-lg leading-8 bg-[#ffeedc] px-56 pt-28 pb-48">
         <div className="pt-7 grid grid-cols-3 gap-x-10">
-          <div className="bg-[#E8B86D] p-10 rounded-xl">
+          <div className="bg-[#E8B86D] p-10 rounded-xl mx-5">
             <div className="flex flex-col">
               <h2 className="text-xl font-semibold">Visi Kami</h2>
               <p className="pt-1">
@@ -64,7 +64,7 @@ const AboutUs = () => {
             </div>
           </div>
 
-          <div className="bg-[#E8B86D] p-10 rounded-xl">
+          <div className="bg-[#E8B86D] p-10 rounded-xl mx-5">
             <div className="flex flex-col">
               <h2 className="text-xl font-semibold">Misi</h2>
               <p className="pt-1">
@@ -83,7 +83,7 @@ const AboutUs = () => {
               </p>
             </div>
           </div>
-          <div className="bg-[#E8B86D] p-10 rounded-xl">
+          <div className="bg-[#E8B86D] p-10 rounded-xl mx-5">
             <div className="flex flex-col">
               <h2 className="text-xl font-semibold">Budaya</h2>
               <p className="pt-1">
