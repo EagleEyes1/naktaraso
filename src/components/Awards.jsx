7df18de3ca2bfe8046f51f9a09add57fdd648c41
unknown <fahder7@gmail.com>
2024-10-06 15:31:48 +0700

    contact form done

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -2,93 +2,103 @@ import React from "react";
 
 const Awards = () => {
   return (
-    <div className="pt-14 mx-auto px-52 bg-[#fff8ee]">
+    <div className="pt-14 px-5 mx-auto bg-[#fff8ee] xl:px-40 lg:px-16 md:px-28 sm:px-14">
       <h1 className="text-3xl font-semibold pb-12 text-center">Penghargaan</h1>
-
-      <div className="grid grid-cols-3 gap-x-8 pb-12">
-        <div className="flex flex-col text-center items-center justify-center relative">
+      <div className="grid grid-cols-2 pb-20 xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 gap-8 xl:pb-20 lg:pb-20 md:pb-16 sm:pb-16">
+        <div className="flex flex-col text-center items-center justify-center relative group z-10">
           <img
             className="h-full w-full mb-3 object-cover relative rounded-3xl"
             src={require("../assets/award1.jpg")}
             alt="award1"
           />
-          <div className="bg-[#E8B86D] p-4 absolute bottom-12 left-0 rounded-r-2xl ">
+          <div className="bg-[#E8B86D] text-xs p-1 xl:p-4 lg:p-2 md:p-4 absolute bottom-8 xl:bottom-12 lg:bottom-12 md:bottom-12 sm:bottom-12 left-0 rounded-r-2xl mr-10 xl:text-md lg:text-sm md:text-sm transition-opacity duration-300 opacity-100 group-hover:opacity-50">
             <h2>Juara 1 Wirausaha Muda tahun 2019</h2>
           </div>
         </div>
-        <div className="flex flex-col text-center items-center justify-center relative">
+        <div className="flex flex-col text-center items-center justify-center relative group z-10">
           <img
             className="h-full w-full mb-3 object-cover relative rounded-3xl"
             src={require("../assets/award2.jpeg")}
             alt="award2"
           />
-          <div className="bg-[#E8B86D] p-4 absolute bottom-12 left-0 rounded-r-2xl">
-            <h2>Juara 1 Wirausaha Muda tahun 2019</h2>
+          <div className="bg-[#E8B86D] text-xs p-1 xl:p-4 lg:p-2 md:p-4 absolute bottom-8 xl:bottom-12 lg:bottom-12 md:bottom-12 sm:bottom-12 left-0 rounded-r-2xl mr-10 xl:text-md lg:text-sm md:text-sm transition-opacity duration-300 opacity-100 group-hover:opacity-50">
+            <h2>Wirausaha Terinovatif 2019</h2>
           </div>
         </div>
-        <div className="flex flex-col text-center items-center justify-center relative">
+        <div className="flex flex-col text-center items-center justify-center relative group z-10">
           <img
             className="h-full w-full mb-3 object-cover relative rounded-3xl"
             src={require("../assets/award3.jpeg")}
             alt="award3"
           />
-          <div className="bg-[#E8B86D] p-4 absolute bottom-12 left-0 rounded-r-2xl">
-            <h2>Juara 1 Wirausaha Muda tahun 2019</h2>
+          <div className="bg-[#E8B86D] text-xs p-1 xl:p-4 lg:p-2 md:p-4 absolute bottom-8 xl:bottom-12 lg:bottom-12 md:bottom-12 sm:bottom-12 left-0 rounded-r-2xl mr-10 xl:text-md lg:text-sm md:text-sm transition-opacity duration-300 opacity-100 group-hover:opacity-50">
+            <h2>Wirausaha Muda terbaik 2019</h2>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-4 gap-x-8 pb-32">
-        <div className="flex flex-col text-center items-center justify-center relative">
+        <div className="flex flex-col text-center items-center justify-center relative group z-10">
           <img
             className="h-full w-full mb-3 object-cover relative rounded-3xl"
             src={require("../assets/award4.jpeg")}
             alt="award4"
           />
-          <div className="bg-[#E8B86D] p-4 absolute bottom-12 left-0 rounded-r-2xl">
-            <h2>Juara 1 Wirausaha Muda tahun 2019</h2>
+          <div className="bg-[#E8B86D] text-xs p-1 xl:p-4 lg:p-2 md:p-4 absolute bottom-8 xl:bottom-12 lg:bottom-12 md:bottom-12 sm:bottom-12 left-0 rounded-r-2xl mr-10 xl:text-md lg:text-sm md:text-sm transition-opacity duration-300 opacity-100 group-hover:opacity-50">
+            <h2>Juara 1 Wirausaha Muda Kab. Solok 2022</h2>
           </div>
         </div>
-        <div className="flex flex-col text-center items-center justify-center relative">
+        <div className="flex flex-col text-center items-center justify-center relative group z-10">
           <img
             className="h-full w-full mb-3 object-cover relative rounded-3xl"
             src={require("../assets/award5.jpeg")}
             alt="award5"
           />
-          <div className="bg-[#E8B86D] p-4 absolute bottom-12 left-0 rounded-r-2xl">
-            <h2>Juara 1 Wirausaha Muda tahun 2019</h2>
+          <div className="bg-[#E8B86D] text-xs p-1 xl:p-4 lg:p-2 md:p-4 absolute bottom-8 xl:bottom-12 lg:bottom-12 md:bottom-12 sm:bottom-12 left-0 rounded-r-2xl mr-10 xl:text-md lg:text-sm md:text-sm transition-opacity duration-300 opacity-100 group-hover:opacity-50">
+            <h2>Finalis Terbaik Apresiasi Kreasi Indonesia (AKI) 2022</h2>
           </div>
         </div>
-        <div className="flex flex-col text-center items-center justify-center relative">
+        <div className="flex flex-col text-center items-center justify-center relative group z-10">
           <img
             className="h-full w-full mb-3 object-cover relative rounded-3xl"
             src={require("../assets/award6.jpg")}
             alt="award6"
           />
-          <div className="bg-[#E8B86D] p-4 absolute bottom-12 left-0 rounded-r-2xl">
-            <h2>Juara 1 Wirausaha Muda tahun 2019</h2>
+          <div className="bg-[#E8B86D] text-xs p-1 xl:p-4 lg:p-2 md:p-4 absolute bottom-8 xl:bottom-12 lg:bottom-12 md:bottom-12 sm:bottom-12 left-0 rounded-r-2xl mr-10 xl:text-md lg:text-sm md:text-sm transition-opacity duration-300 opacity-100 group-hover:opacity-50">
+            <h2>Finalis Terbaik Indonesia Food Inovation 2022</h2>
           </div>
         </div>
-        <div className="flex flex-col text-center items-center justify-center relative">
+        <div className="flex flex-col text-center items-center justify-center relative group z-10">
           <img
             className="h-full w-full mb-3 object-cover relative rounded-3xl"
             src={require("../assets/award7.jpg")}
             alt="award7"
           />
-          <div className="bg-[#E8B86D] p-4 absolute bottom-12 left-0 rounded-r-2xl">
-            <h2>Juara 1 Wirausaha Muda tahun 2019</h2>
+          <div className="bg-[#E8B86D] text-xs p-1 xl:p-4 lg:p-2 md:p-4 absolute bottom-8 xl:bottom-12 lg:bottom-12 md:bottom-12 sm:bottom-12 left-0 rounded-r-2xl mr-10 xl:text-md lg:text-sm md:text-sm transition-opacity duration-300 opacity-100 group-hover:opacity-50">
+            <h2>
+              Finalis Terbaik Apresiasi Kreasi Indonesia (AKI) Sumatera Barat
+              2022
+            </h2>
+          </div>
+        </div>
+        <div className="flex flex-col text-center items-center justify-center relative group z-10">
+          <img
+            className="h-full w-full mb-3 object-cover relative rounded-3xl"
+            src={require("../assets/award8.jpeg")}
+            alt="award7"
+          />
+          <div className="bg-[#E8B86D] text-xs p-1 xl:p-4 lg:p-2 md:p-4 absolute bottom-8 xl:bottom-12 lg:bottom-12 md:bottom-12 sm:bottom-12 left-0 rounded-r-2xl mr-10 xl:text-md lg:text-sm md:text-sm transition-opacity duration-300 opacity-100 group-hover:opacity-50">
+            <h2>Booth Terbaik Pada Selangor International Summit Expo 2019</h2>
           </div>
         </div>
       </div>
       <div
         aria-hidden="true"
-        className="absolute inset-x-0 top-[calc(100%-13rem)] z-0 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem) xl:top-[calc(100%-40rem)]"
+        className="absolute inset-x-0 top-[calc(100%-13rem)] z-0 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem) xl:top-[calc(100%-40rem)] lg:top-[calc(100%-40rem)] md:top-[calc(100%-20rem)]"
       >
         <div
           style={{
             clipPath:
               "polygon(11% 100%, 11% 56%, 6% 48%, 2% 44%, 0% 46%, 0% 89%)",
           }}
-          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff0400] to-[#ff9300] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem] xl:left-[calc(50%-20rem)]"
+          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff0400] to-[#ff9300] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem] xl:left-[calc(50%-20rem)] lg:left-[calc(50%-8rem)]"
         />
       </div>
     </div>
--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import emailjs from "@emailjs/browser";
 
 const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    console.log("halo");
+    emailjs
+      .sendForm("service_7f99m1t", "template_6ijswa5", form.current, {
+        publicKey: "Y3WsT7DrGPel8vYrn",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <div className="mt-14 mx-auto px-52">
       <h1 className="text-5xl font-extrabold pb-12">Kontak Kami</h1>
@@ -76,102 +97,52 @@ const ContactUs = () => {
           <div className="bg-[#E8B86D] py-10 rounded-xl">
             <h1 className="text-center pb-8">Formulir Kontak</h1>
 
-            <form class="max-w-md mx-auto">
+            <form ref={form} onSubmit={sendEmail} class="max-w-md mx-auto">
+              <div class="relative z-0 w-full mb-5 group">
+                <input
+                  type="text"
+                  name="fname"
+                  id="fname"
+                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-black peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  for="fname"
+                  class="peer-focus:font-medium absolute text-sm text-black dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Nama Kamu
+                </label>
+              </div>
               <div class="relative z-0 w-full mb-5 group">
                 <input
                   type="email"
-                  name="floating_email"
-                  id="floating_email"
+                  name="email"
+                  id="email"
                   class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-black peer"
                   placeholder=" "
                   required
                 />
                 <label
-                  for="floating_email"
+                  for="email"
                   class="peer-focus:font-medium absolute text-sm text-black dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  Email address
+                  Email
                 </label>
-              </div>
-              <div class="relative z-0 w-full mb-5 group">
-                <input
-                  type="password"
-                  name="floating_password"
-                  id="floating_password"
-                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-black peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="floating_password"
-                  class="peer-focus:font-medium absolute text-sm text-black dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Password
-                </label>
-              </div>
-              <div class="relative z-0 w-full mb-5 group">
-                <input
-                  type="password"
-                  name="repeat_password"
-                  id="floating_repeat_password"
-                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-black peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="floating_repeat_password"
-                  class="peer-focus:font-medium absolute text-sm text-black dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Confirm password
-                </label>
-              </div>
-              <div class="grid md:grid-cols-2 md:gap-6">
-                <div class="relative z-0 w-full mb-5 group">
-                  <input
-                    type="text"
-                    name="floating_first_name"
-                    id="floating_first_name"
-                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-black peer"
-                    placeholder=" "
-                    required
-                  />
-                  <label
-                    for="floating_first_name"
-                    class="peer-focus:font-medium absolute text-sm text-black dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                  >
-                    First name
-                  </label>
-                </div>
-                <div class="relative z-0 w-full mb-5 group">
-                  <input
-                    type="text"
-                    name="floating_last_name"
-                    id="floating_last_name"
-                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-black peer"
-                    placeholder=" "
-                    required
-                  />
-                  <label
-                    for="floating_last_name"
-                    class="peer-focus:font-medium absolute text-sm text-black dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                  >
-                    Last name
-                  </label>
-                </div>
               </div>
               <div class="grid md:grid-cols-2 md:gap-6">
                 <div class="relative z-0 w-full mb-5 group">
                   <input
                     type="tel"
                     pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                    name="floating_phone"
-                    id="floating_phone"
+                    name="phone"
+                    id="phone"
                     class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-black peer"
                     placeholder=" "
                     required
                   />
                   <label
-                    for="floating_phone"
+                    for="phone"
                     class="peer-focus:font-medium absolute text-sm text-black dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
                     Phone number (123-456-7890)
@@ -180,20 +151,37 @@ const ContactUs = () => {
                 <div class="relative z-0 w-full mb-5 group">
                   <input
                     type="text"
-                    name="floating_company"
-                    id="floating_company"
+                    name="company"
+                    id="company"
                     class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-black peer"
                     placeholder=" "
                     required
                   />
                   <label
-                    for="floating_company"
+                    for="company"
                     class="peer-focus:font-medium absolute text-sm text-black dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
                     Company (Ex. Google)
                   </label>
                 </div>
               </div>
+              <div class="relative z-0 w-full mb-5 group">
+                <textarea
+                  rows={5}
+                  name="message"
+                  id="message"
+                  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-black appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-black peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  for="message"
+                  class="peer-focus:font-medium absolute text-sm text-black dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-black peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Pesan
+                </label>
+              </div>
+
               <button
                 type="submit"
                 class="bg-[#ffeedc] hover:bg-[#ffeedc]/80 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
